01410030ef34ca6025aab4e9f15899b5aa7b541f
Kiwi <42769725+KiwiB0y@users.noreply.github.com>
2020-12-30 19:50:54 +0100

    Uploaded my website

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function actionGithub(){
+    window.open(
+	"https://github.com/KiwiB0y", "_blank"
+    );
+}
+
+function actionLinked(){
+    window.open(
+	"https://www.linkedin.com/in/tsvyatko-hristov-745317201/", "_blank"
+    );
+}
+
+
+function normalGit() {
+    document.getElementById("github-icon").style.transform = "scale(1.0)";
+}
+
+function normalLinked() {		
+    document.getElementById("linked-icon").style.transform = "scale(1.0)";
+}
+
+function hoverGit() {
+    document.getElementById("github-icon").style.transform = "scale(1.6)";
+}		
+
+function hoverLinked() {		
+    document.getElementById("linked-icon").style.transform = "scale(1.6)";
+}
