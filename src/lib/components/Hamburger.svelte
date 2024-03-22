d470f0f8d3066b37cd052d3c07ede477fc42c9f0
kiwib0y <caki6ko@yahoo.com>
2024-03-22 15:59:29 +0100

    Add Hamburger.svelte and update Navbar
      The only thing missing is the hamburger menu functionality, though
      the hamburger menu is already on.

---
+++ src/lib/components/Hamburger.svelte
@@ -0,0 +1,71 @@
+<script>
+  export let open = false;
+</script>
+
+<div class="button-container">
+  <button
+    aria-label="burger"
+    class="burger"
+    class:open
+    on:click={() => (open = !open)}
+    >
+    <svg width="30" height="16">
+      <line id="top" x1="0" y1="2" x2="32" y2="2" />
+      <line id="middle" x1="0" y1="12" x2="32" y2="12" />
+      <line id="bottom" x1="0" y1="22" x2="32" y2="22" />
+    </svg>
+  </button>
+</div>
+
+
+<style>
+  svg {
+    min-height: 24px;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  svg line {
+    stroke: var(--fgColor);
+    stroke-width: 3;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  button {
+    color: var(--fgColor);
+    background: transparent;
+    cursor: pointer;
+    border: none;
+    padding: 0.5rem;
+  }
+
+  .open svg {
+    min-height: 24px;
+    transform: scale(1.2);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .open #top {
+    transform: translate(6px, 0px) rotate(45deg);
+  }
+
+  .open #middle {
+    stroke-dasharray: 1000;
+    stroke-dashoffset: 1000;
+  }
+
+  .open #bottom {
+    transform: translate(-12px, 9px) rotate(-45deg);
+  }
+
+  @keyframes fade {
+    to {
+      stroke-dashoffset: 1000;
+      stroke-dasharray: 1000;
+    }
+  }
+</style>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,51 +1,86 @@
 <script>
   import routes from '$lib/Routes.js';
+  import Hamburger from './Hamburger.svelte';
+
   export let path;
+
+  let opened = false;
+
+  function toggleNavbar() {
+    opened = !opened;
+  }
 </script>
 
-<div class="buttons">
-	{#each routes as route}
-    {#if path === route.href}
-      <a class="button selected" href={route.href}>
-        {route.label}
-      </a>
-    {:else}
-      <a class="button" href={route.href}>
-        {route.label}
-      </a>
-    {/if}
-  {/each}
+<div class="navbar">
+  <div class="burger">
+    <Hamburger bind:open={opened} />
+  </div>
+  <div class="buttons">
+    {#each routes as route}
+      {#if path === route.href}
+        <a class="button selected" href={route.href}>
+          {route.label}
+        </a>
+      {:else}
+        <a class="button" href={route.href}>
+          {route.label}
+        </a>
+      {/if}
+    {/each}
+  </div>
 </div>
 
 <style>
+  .navbar {
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 15rem;
+  }
+
   .buttons {
-    position: relative;
-    left: 1080px;
+    position: absolute;
     display: flex;
     justify-content: space-between;
     align-items: center;
-		max-width: 100px;
-		width: 100%;
-		text-align: center;
+    max-width: 100px;
+    width: 100%;
+    text-align: center;
     margin-top: 20px;
-	}
+  }
 
   .buttons :global(a) {
-		color: var(--fgColor);
-	}
+    color: var(--fgColor);
+  }
 
   .buttons .button {
     padding: 0;
     display: block;
     cursor: pointer;
     text-decoration: none;
-    position: block;
     margin: 10px;
     font-size: 20px;
   }
-  
+
   .button:hover {
-		transition: color 0.4s ease-in-out;
+    transition: color 0.4s ease-in-out;
     color: var(--hoverColor);
-	}
+  }
+
+  @media (min-width: 900px) {
+    .burger {
+      display: none !important;
+    }
+  }
+
+  @media (max-width: 900px) {
+    .buttons .button{
+      display: none !important;
+    }
+
+    .navbar {
+      display: flex;
+      justify-content: flex-end;
+      margin-right: 2rem;
+    }
+  }
 </style>
