62fd89c3c945dd4cc4e096b740be6725e16c9ebc
tsakishii <ts.chris@protonmail.com>
2025-10-26 21:20:51 +0100

    update: Both svelte and vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
-	kit: {
-    appDir: 'app', // Required as the default is _app
-    adapter: adapter()
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
   }
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit({
+      configFile: 'svelte.config.js'
+    })
+  ]
 });
