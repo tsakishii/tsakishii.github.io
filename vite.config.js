717e84dcf1f3d9c8f5f733f474d962686abde74f
tsakishii <ts.chris@protonmail.com>
2026-08-30 14:47:01 +0200

    update: vite.config.js & svelte.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [
-    sveltekit({
-      configFile: 'svelte.config.js'
-    })
-  ]
+  plugins: [sveltekit()]
 });
